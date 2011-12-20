2bdd8bfde8e42794c1d8fed2043b7caba604dbd1
denismurashev <DenisMurashev@gmail.com>
2011-12-20 13:52:49 +0000

    tfi-dao version 1.0 released

--- tfi-docs/src/main/java/ru/sstu/docs/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>TFI Project for Documents.</p>
- * <p>This package provides classes for working with different types of
- * documents.</p>
- */
-package ru.sstu.docs;
--- tfi-xml/src/test/java/ru/sstu/xml/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Tests for {@link ru.sstu.xml} package.
- */
-package ru.sstu.xml;
