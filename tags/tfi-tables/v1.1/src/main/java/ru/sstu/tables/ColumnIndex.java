add558f4c80075b28823b74befb80eac0cbf7b2f
denismurashev@gmail.com <denismurashev@gmail.com@6ae4eb0a-7d8b-1a81-3175-6f1955b5273f>
2012-01-03 11:18:32 +0000

    tfi-tables version 1.1 released

---
+++ tags/tfi-tables/v1.1/src/main/java/ru/sstu/tables/ColumnIndex.java
@@ -0,0 +1,27 @@
+package ru.sstu.tables;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * <code>ColumnIndex</code> annotation is used for marking Java Bean fields
+ * as columns in table.
+ *
+ * @author Denis_Murashev
+ * @since Tables 1.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ColumnIndex {
+
+	/**
+	 * Index of column.
+	 */
+	int value();
+
+	/**
+	 * Default value of a column.
+	 *
+	 * @since Tables 1.1
+	 */
+	String defaultValue() default "";
+}
---
+++ tags/tfi-tables/v1.1/src/main/java/ru/sstu/tables/TableException.java
@@ -0,0 +1,42 @@
+package ru.sstu.tables;
+
+/**
+ * <code>DataException</code> means correct data cannot be accessed.
+ *
+ * @author Denis_Murashev
+ */
+public class TableException extends Exception {
+
+	/**
+	 * Serial version UID.
+	 */
+	private static final long serialVersionUID = 479646188055449354L;
+
+	/**
+	 * Constructs a new exception with the specified detail message.  The
+	 * cause is not initialized, and may subsequently be initialized by
+	 * a call to {@link #initCause}.
+	 *
+	 * @param message the detail message. The detail message is saved for
+	 *          later retrieval by the {@link #getMessage()} method.
+	 */
+	public TableException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructs a new exception with the specified cause and a detail
+	 * message of <tt>(cause==null ? null : cause.toString())</tt> (which
+	 * typically contains the class and detail message of <tt>cause</tt>).
+	 * This constructor is useful for exceptions that are little more than
+	 * wrappers for other throwables.
+	 *
+	 * @param cause the cause (which is saved for later retrieval by the
+	 *         {@link #getCause()} method).  (A <tt>null</tt> value is
+	 *         permitted, and indicates that the cause is nonexistent or
+	 *         unknown.)
+	 */
+	public TableException(Throwable cause) {
+		super(cause);
+	}
+}
