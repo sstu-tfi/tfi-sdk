36be58d595d0dc64a779cbf1a8ab278848071e64
denismurashev <DenisMurashev@gmail.com>
2012-12-30 10:40:47 +0000

    [Web] CharacterEncodingFilter was added

---
+++ tfi-web/src/main/java/ru/sstu/web/core/CharacterEncodingFilter.java
@@ -0,0 +1,35 @@
+package ru.sstu.web.core;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * {@code CharacterEncodingFilter} class is web filter for fixing encoding
+ * problems.
+ *
+ * @author Denis_Murashev
+ * @since Web 1.1
+ */
+public class CharacterEncodingFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
